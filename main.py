b73046b3ca7053c19370597e10a9fa79dfa72e5a
jose.francisco.udaeta <jose.francisco.udaeta@est.fib.upc.edu>
2022-07-07 21:12:13 +0200

    [ADD] Ahora ya puedo sacar frames individuales

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 import sys
 import cv2
 import numpy as np
@@ -7,23 +8,37 @@ FRAMES_PER_SECOND = 1
 
 def usage (argv):
     if len(argv) != 2:
-        error_msg = '''
-        Usage: python3 video_to_colors.py path_to_video
-        '''
+        error_msg = '''Usage: python3 video_to_colors.py path_to_video'''
         print(error_msg, file=sys.stderr)
         exit(1)
 
-def extract_average_photo (path_to_video: str) -> str:
-    video_capture = cv2.VideoCapture(path_to_video)
-    fps = video_capture.get(cv.CAP_PROP_FPS)
-    print (fps)
+def average_color (frame):
+    print (type (frame), frame)
+    print ("\n\n\n")
+
+def extract_average_photo (path) -> str:
+    # filename = "/home/cpt/Documents/programming-projects/movie-to-color/alice.avi"
+    filename = path
+    video_capture = cv2.VideoCapture(filename)
+    frame_count = 0
+    image_count = 0
+    
+    while True:
+        ret, frame = video_capture.read()
+        if ret == False:
+            break
+        if frame_count % 25 == 0:
+            average_color (frame) 
+            image_count = image_count + 1
+        frame_count = frame_count + 1
+    
+    return True
 
 
 def main (argv):
     usage (argv)
-    path = extract_average_photo(str)
+    path = extract_average_photo(argv[1])
 
 
 if __name__ == '__main__':
     main (sys.argv)
-
